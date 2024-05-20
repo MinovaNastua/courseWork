13170a9e465d5c952eb46b0fd390e5943e30ac5a
minova.nastionnnna@gmail.com <minova.nastionnnna@gmail.com>
2024-05-20 09:42:07 +0300

    feat: add project

---
+++ scriptFormYpROJECT.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    let login = document.getElementById('requestFrameButton');
+    login.addEventListener('click', function() {
+        window.location.href ="C:/веб/course/index.html";
+    });
+
+    let myPrFrameTub = document.getElementById('myPrFrameTub');
+    myPrFrameTub.addEventListener('click', function() {
+        
+        window.location.href ="C:/веб/course/htmlForClient.html";
+    });
+    });
